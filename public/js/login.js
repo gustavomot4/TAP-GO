22defd836798df9a8aaee0635c9fc1de6aa1427b
gustavomot4 <gustavo.mota@sptech.school>
2025-10-28 23:25:27 -0300

    Estiilização tela de login e cadastro

---
+++ public/js/login.js
@@ -0,0 +1,16 @@
+function Alternar() {
+    if (container_cadastro_visivel.style.display == `none`) {
+        container_cadastro_visivel.style.display = `block`;
+        container_login_visivel.style.display = `none`;
+    } else {
+        container_cadastro_visivel.style.display = `none`;
+        container_login_visivel.style.display = `block`;
+    }
+}
+
+function Login() {
+}
+
+function Cadastrar() {
+}
+
